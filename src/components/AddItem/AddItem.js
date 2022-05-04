3033f40d1bd6d13247fd45f98f7ab24a0eade750
MuhammadJamshaidGhaffar <muhammadjamshaid89@gmail.com>
2022-05-04 10:42:18 +0500

    Added Input and Btn

--- src/components/AddItem/AddItem.js
+++ src/components/AddItem/AddItem.js
@@ -4,8 +4,24 @@ import styles from "./AddItem.module.css";
 const AddItem = ({ style }) => (
   <div style={style} className={styles.mainContainer}>
     <h2 className={styles.heading}>Add New Transaction</h2>
-    <label className={styles.label}>Text</label>
-    <input type="text" placeholder="Enter Text"></input>
+    <div className={styles.inputWrapper}>
+      <input
+        class={styles.input}
+        id="text"
+        type="text"
+        placeholder="Enter Text"
+      />
+      <div className={styles.numberInputWrapper}>
+        <input
+          class={styles.input}
+          id="amount"
+          type="number"
+          placeholder="Enter Amount"
+        />
+        <p>-ve for Expense and +ve for Income</p>
+      </div>
+    </div>
+    <button className={styles.addBtn}>Add Transaction</button>
   </div>
 );
 
--- src/components/History/HistoryElm/HistoryElm.js
+++ src/components/History/HistoryElm/HistoryElm.js
@@ -2,7 +2,8 @@ import React from "react";
 import styles from "./HistoryElm.module.css";
 
 const HistoryElm = ({ style, name, value }) => {
-  let valueColor = value > 0 ? { color: "green" } : { color: "red" };
+  let valueColor =
+    value > 0 ? { color: "rgb(0, 211, 0)" } : { color: "rgb(255, 0, 0)" };
   return (
     <div className={styles.mainContainer} style={style}>
       <p>{name}</p>
